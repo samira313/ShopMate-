dceab016882d7445ce1bca34744b9f6b29842705
Samira <ahmadi.samira6761@gmail.com>
2025-08-08 15:35:33 +0200

    implement logout with redirect to login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../services/authService";
 import "../styles/Navbar.css"; 
 
 export default function Navbar() {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            navigate("/login");
+
+        }catch(error){
+            console.error("Logout failed:", error)
+        }
+    }
   return (
     <nav className="navbar">
       <h1 className="navbar-title">ShopMate</h1>
@@ -11,7 +22,7 @@ export default function Navbar() {
       <div className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
-        <button className="logout-btn" onClick={logout}>Logout</button>
+        <button className="logout-btn" onClick={handleLogout}>Logout</button>
       </div>
     </nav>
   );
