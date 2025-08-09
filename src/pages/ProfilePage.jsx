ef8752dc1f3a7c3cdfae1ea4dc622eaaea3b446a
Samira <ahmadi.samira6761@gmail.com>
2025-08-09 12:35:39 +0200

    improved profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,10 +6,10 @@ export default function ProfilePage() {
 
     const  {currentUser}= useAuth();
     if (!currentUser) 
-        return <p>Loading ...</p>
+        return <p>Please log in to view your profile</p>
   return (
     <div className='profile-container'>
-        <h2>Profile Page</h2>
+        <h2>Profile</h2>
         <p>Email: <strong> {currentUser.email}</strong></p>
         <p>User ID:<strong> {currentUser.uid}</strong> </p>
     </div>
