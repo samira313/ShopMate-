631c737aba4fe8a4cebac55e0f41e9fc6d3576a3
Samira <ahmadi.samira6761@gmail.com>
2025-09-03 16:05:39 +0200

    added local network IP for testing on mobile devices

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
+import { initAuthPersistence } from './services/authService.js'
 
 registerSW()
-
+initAuthPersistence();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
